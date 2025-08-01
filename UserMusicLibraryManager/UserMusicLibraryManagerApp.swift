6c56b1b0c8dfb77072329520e07fd45fccb0e21b
Austin Krum <atk1clayb2@gmail.com>
2025-08-01 00:27:59 -0400

    Initial Commit

---
+++ UserMusicLibraryManager/UserMusicLibraryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserMusicLibraryManagerApp.swift
+//  UserMusicLibraryManager
+//
+//  Created by Austin Krum on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct UserMusicLibraryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserMusicLibraryManagerTests/UserMusicLibraryManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserMusicLibraryManagerTests.swift
+//  UserMusicLibraryManagerTests
+//
+//  Created by Austin Krum on 8/1/25.
+//
+
+import Testing
+@testable import UserMusicLibraryManager
+
+struct UserMusicLibraryManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
